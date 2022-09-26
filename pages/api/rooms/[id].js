33fa8c538ac0d0e834d727f32736ccfc860041c5
Radu <radu.avadanii@bludelego.it>
2022-09-26 10:19:55 +0300

    feat: room controller

---
+++ pages/api/rooms/[id].js
@@ -0,0 +1,20 @@
+import nc from "next-connect";
+import dbConnect from "../../../config/dbConnect";
+
+import {
+   getSingleRoom,
+   updateRoom,
+   deleteRoom,
+} from "../../../controllers/roomControllers";
+
+const handler = nc();
+
+dbConnect();
+
+handler.get(getSingleRoom);
+
+handler.put(updateRoom);
+
+handler.delete(deleteRoom);
+
+export default handler;
