cb4c262aa312e1643e379f5a25e839a0b44fcb17
Radu <radu.avadanii@bludelego.it>
2022-09-27 10:03:10 +0300

    init: search api

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -6,7 +6,19 @@ const errMiddleware = (err, req, res, next) => {
    let error = { ...err };
    error.message = err.message;
 
-   res.status(err.statusCode).json({
+   // Wrong Mongoose Object ID Error
+   if (err.name === "CastError") {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+   }
+
+   // Handling Mongoose Validation Error
+   if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+   }
+
+   res.status(error.statusCode).json({
       success: false,
       error,
       message: error.message,
