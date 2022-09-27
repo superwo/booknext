9d4df6f1c2a4178c194ab44293ac5d26eb1b7776
Radu <radu.avadanii@bludelego.it>
2022-09-27 12:58:35 +0300

    init: search

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -6,7 +6,9 @@ import APIFeatures from "../utils/apiFeatures";
 
 // Get all rooms => /api/rooms
 const allRooms = catchAsyncErrors(async (req, res) => {
-   const apiFeatures = new APIFeatures(Room.find(), req.query).search();
+   const apiFeatures = new APIFeatures(Room.find(), req.query)
+      .search()
+      .filter();
 
    const rooms = await apiFeatures.query;
 
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -14,9 +14,25 @@ class APIFeatures {
            }
          : {};
 
-      console.log(location);
-
       this.query = this.query.find({ ...location });
+      return this;
+   }
+
+   filter() {
+      const queryCopy = { ...this.queryStr };
+
+      // Removing fields from the query
+      const removeFields = ["location", "page"];
+      removeFields.forEach((el) => delete queryCopy[el]);
+
+      // Advance filter for price, ratings etc
+      let queryStr = JSON.stringify(queryCopy);
+      queryStr = queryStr.replace(
+         /\b(gt|gte|lt|lte)\b/g,
+         (match) => `$${match}`
+      );
+
+      this.query = this.query.find(JSON.parse(queryStr));
       return this;
    }
 }
